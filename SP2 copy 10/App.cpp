5edf949065e4f025fbf8f4c45b571888791f00be
Axel3246 <hplaxel@gmail.com>
2021-11-01 13:03:55 -0600

    SP2 - BACKUP & TEST 10
    Discarded

---
+++ SP2 copy 10/App.cpp
@@ -0,0 +1,335 @@
+/*
+Nombre: Eduardo Loya Montes de Oca - A01383049
+Nombre: Roberto Abraham Perez Iga - A01384237
+Nombre: Axel Amos Hernández Cárdenas - A00829837
+Fecha de Creación: 04 de Septiembre del 2021
+
+Ultima Versión: 
+
+Descripción: El canal de Suez es un canal navegable que conecta el mar 
+Mediterráneo con el mar Rojo a través de alrededor de 190 km, uniendo por 
+un el lado del mar Mediterráneo desde el puerto Said hasta el golfo de Suez en 
+el mar Rojo. Este canal es navegado entre 49 y 97 barcos diariamente.
+
+Objetivos:
+1. Solicite el nombre del archivo de entrada (Ej. canal.txt) y lo abra, almacenando los datos en un vector.
+2. Ordene la información por UBI + Fecha (primero por UBI, al haber empate ordenar por fecha).
+3. Solicite al usuario la serie a buscar (los primeros tres caracteres de el UBI).
+4. Despliegue todas las entradas al canal de los buques de esas series en forma ordenada UBI+Fecha.
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// * Struct para el vector que guardara el .txt file
+struct registro
+{
+    string fecha;
+    string mes;
+    string year;
+    string hora;
+    char entrada;
+    string ubi;
+    int fechaInt;
+    bool operator>(const registro &otro) const
+    {
+        if (this->ubi == otro.ubi)
+        {
+            return this->fechaInt > otro.fechaInt;
+        }
+        return (this->ubi.compare(otro.ubi) > 0); // * Cuidado como compara 2 strings
+    }
+
+    string ubisub(int i)
+    {
+        string ubic = ubi.substr(0, i);
+        return ubic;
+    }
+
+    friend ostream &operator<<(ostream &os, const registro &otra);
+};
+
+struct registroVect
+{
+    string mes;
+    string year;
+    int cantMed;
+    int cantRoj;
+    int mesInt;
+
+    
+};
+
+#include "LL.h"
+
+// * Funcion que sobrecarga el operator <<
+ostream &operator<<(ostream &os, const registro &otra)
+{
+    // Instrucciones
+    os << otra.ubi << " " << otra.fecha << " " << otra.mes << " " << otra.year << " " << otra.hora << " " << otra.entrada << endl;
+    return os;
+}
+
+// * Funcion que transforma un mes a INT
+// ? Complejidad: O(1)
+int mestoi(string m)
+{
+
+    if (m == "jan")
+    {
+
+        return 1;
+    }
+    if (m == "feb")
+    {
+
+        return 2;
+    }
+    if (m == "mar")
+    {
+
+        return 3;
+    }
+    if (m == "apr")
+    {
+
+        return 4;
+    }
+    if (m == "may")
+    {
+
+        return 5;
+    }
+    if (m == "jun")
+    {
+
+        return 6;
+    }
+    if (m == "jul")
+    {
+
+        return 7;
+    }
+    if (m == "aug")
+    {
+
+        return 8;
+    }
+    if (m == "sep")
+    {
+
+        return 9;
+    }
+    if (m == "oct")
+    {
+
+        return 10;
+    }
+    if (m == "nov")
+    {
+
+        return 11;
+    }
+    return 12;
+}
+
+// * Funcion que transforma una fecha string a INT
+// ? Complejidad: O(n)
+int ftoi(string fecha)
+{
+
+    int salida = 0;
+    int aux = 0;
+    int i = 0;
+
+    while (fecha[i] != '-')
+    {
+
+        aux *= 10;
+        aux += (fecha[i] - '0'); // * '5' -'0' == 5 como entero (53-48)
+        i++;
+    }
+
+    salida += aux;
+    i++;
+    string mes = "";
+
+    while (fecha[i] != '-')
+    {
+
+        mes += fecha[i];
+        i++;
+    }
+
+    salida += (mestoi(mes) * 100);
+    aux = 0;
+    i++;
+
+    while (i < fecha.length())
+    {
+
+        aux *= 10;
+        aux += (fecha[i] - '0'); // '5' -'0' == 5 como entero (53-48)
+        i++;
+    }
+
+    salida += (aux * 10000);
+    return salida;
+}
+
+vector<registroVect> finalRegister(vector<registroVect> vec1, vector<registroVect> vec2){
+
+
+    // comp mes
+    //xon - 0
+
+
+
+}
+
+
+int main()
+{
+
+    system("cls"); // * Limpiar la terminal
+
+    string date, hour, ub, nomArch;
+    int dateInt;
+    char entryDate;
+
+    cout << "Archivo -> ";
+    cin >> nomArch;
+    cout << endl;
+
+    while (nomArch != "suez.txt")
+    {
+
+        cout << "Favor de insertar archivo suez.txt -> ";
+        cin >> nomArch;
+        cout << endl;
+    }
+
+    ifstream datosSuez;
+    datosSuez.open("suez.txt");
+
+    LinkedList<registro> buquesM;
+    LinkedList<registro> buquesR;
+
+    while (datosSuez >> date >> hour >> entryDate >> ub)
+    {
+        registro registro;
+        int idx = date.find("-");
+
+        registro.fecha = date;
+        registro.mes = date.substr(idx + 1, 3);
+        registro.year = date.substr(idx + 5, 2);
+        registro.hora = hour;
+        registro.entrada = entryDate;
+        registro.ubi = ub;
+        registro.fechaInt = ftoi(date);
+
+        if (registro.entrada == 'M')
+        {
+
+            buquesM.addLast(registro);
+        }
+        else
+        {
+            buquesR.addLast(registro);
+        }
+    }
+
+    datosSuez.close();
+
+    cout << "Ordenando Datos..." << endl;
+    cout << endl;
+
+    buquesM.sort();
+    buquesR.sort();
+
+    string archSalida;
+    cout << "Nombre de archivo de salida para buques del Mar Mediterraneo -> ";
+    cin >> archSalida;
+    buquesM.fileout(archSalida);
+
+    cout << "Nombre de archivo de salida para buques del Mar Rojo -> ";
+    cin >> archSalida;
+    buquesR.fileout(archSalida);
+
+    // en el cuarto se puede usar vector
+    string serieSearch;
+
+    cout << "Serie a buscar -> ";
+    cin >> serieSearch;
+
+    //buquesR.print();
+    //buquesM.print();
+    vector<registroVect> vec1 = buquesM.busqueda(serieSearch);
+/*
+    cout << "----------------------------Mediterraneo-------------------" << endl;
+
+    for (int i = 0; i < vec1.size(); i++)
+    {
+        cout << vec1[i].mes << " " << vec1[i].year << " " << vec1[i].cantMed << " " << vec1[i].cantRoj << endl;
+    }
+
+    vector<registroVect> vec2 = buquesR.busqueda(serieSearch);
+
+    cout << "----------------------------Rojo-------------------" << endl;
+
+    for (int i = 0; i < vec2.size(); i++)
+    {
+        cout << vec2[i].mes << " " << vec2[i].year << " " << vec2[i].cantMed << " " << vec2[i].cantRoj << endl;
+    }
+*/
+/*
+    string meses[12] = {"jan", "feb", "mar", "apr", "may", "jun", "jul", "aug","sep", "oct", "nov", "dic"};
+    vector<registroVect> vectF;
+    int maxsi = max(vec1.size(),vec2.size());
+    int mins = min(vec1.size(),vec2.size());
+    int size = maxsi - mins;
+
+    for(int i = 0; i < size; i++){
+        if((meses[i] == vec1[i].mes) || (meses[i] == vec2[i].mes)){
+            
+            registroVect finalReg;
+            finalReg.mes = meses[i];
+            finalReg.year = vec1[i].year;
+            finalReg.cantMed = vec1[i].cantMed + vec2[i].cantMed;
+            finalReg.cantRoj = vec1[i].cantRoj + vec2[i].cantRoj;
+            
+            vectF.push_back(finalReg);
+        }
+    }
+
+    cout << "----------------------------Vect F-------------------" << endl;
+
+
+    for (int i = 0; i < vectF.size(); i++)
+    {
+        cout << vectF[i].mes << " " << vectF[i].year << " " << vectF[i].cantMed << " " << vectF[i].cantRoj << endl;
+    }
+    */
+
+
+
+
+    
+    LinkedList<registro> buquesT(buquesR);
+    buquesT += buquesM;
+    buquesT.sort();
+
+    vector<registroVect> vectorFin = buquesT.busqueda(serieSearch);
+
+    for (int i = 0; i < vectorFin.size(); i++)
+    {
+        cout << vectorFin[i].mes << " " << vectorFin[i].year << " " << vectorFin[i].cantMed << " " << vectorFin[i].cantRoj << endl;
+    }
+    
+}
